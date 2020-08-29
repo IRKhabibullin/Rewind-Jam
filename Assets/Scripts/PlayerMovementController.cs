50c1fe50f0753f5e017f3ecf96be5bcbb3e4e05c
IRKhabibullin <36387914+IRKhabibullin@users.noreply.github.com>
2020-08-29 20:26:50 +0300

    movement script refactoring

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] private MovementController controller;
+
+    [SerializeField] private float moveSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool isJumping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
+    }
+}
